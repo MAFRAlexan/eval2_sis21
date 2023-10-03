97a339ca62454e30f02fe06a30e497c151b0b2d0
MAFRAlexan <144250135+MAFRAlexan@users.noreply.github.com>
2023-10-03 09:50:48 -0600

    creacion del archivo registrar y agregar codigo al archivo home

---
+++ lib/pages/home.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+
+class home extends StatefulWidget{
+  @override
+  State<home> createState() => homeState();
+}
+
+class homeState extends State<home>{
+  int ItemDrawer = 0;
+
+
+
+  void _onSelectItemDrawer( int pos){
+    Navigator.pop(context);
+    setState(() {
+      ItemDrawer = pos;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('EVALUACIÓN 2'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              child: Center(
+                child: Text(
+                  'MENÚ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+                color: Colors.red
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box_outlined),
+              title: Text('Acerca de nosotros'),
+              onTap: (){
+                _onSelectItemDrawer(0);
+
+              },
+            ),
+            ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Registrar'),
+                onTap: (){
+                  _onSelectItemDrawer(1);
+
+                }
+            ),
+            ListTile(
+                leading: Icon(Icons.construction),
+                title: Text('Consultar'),
+                onTap: (){
+                  _onSelectItemDrawer(2);
+
+                }
+            ),
+            Divider(
+                color: Colors.red
+            ),
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
