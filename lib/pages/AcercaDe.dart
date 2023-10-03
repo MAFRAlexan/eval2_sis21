80459c07690134fbbb26a74565afc395fb0b5172
Carny22 <danielcarniel2003@gmail.com>
2023-10-03 10:12:02 -0600

    Creacion de pagina AcercaDe

---
+++ lib/pages/AcercaDe.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AcercaDe extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return  SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset('.jpeg',
+                    height: 150,
+                    width: 150,),
+                  const Text('Carlos Daniel Cañas Carrillo',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.purple
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset('.jpeg',
+                    height: 150,
+                    width: 150,),
+                  const Text('Manuel Alexander Flores Rivas',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.purple
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+}
